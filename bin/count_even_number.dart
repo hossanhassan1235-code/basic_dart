1e4d2ffbee73220b56a15ed381225edf9dbdc988
Hossam Hassan <hossanhassan1232@gmail.com>
2026-08-29 00:52:57 -0700

    update bank account code

--- bin/count_even_number.dart
+++ bin/count_even_number.dart
@@ -1,9 +1,35 @@
-void main() {
-  List<int> array = [3, 4, 8, 9, 12, 6];
-  int even = 0;     
-  for (int i = 0; i < array.length; i++) {
-    if (array[i] % 2 == 0) {
-      print(array[i]);
+class BankAccount {
+  String accountID;
+  double balance;
+
+  BankAccount(this.accountID, this.balance);
+
+  BankAccount.zero(this.accountID) : balance = 0.0 {}
+
+  void deposit(double amount) {
+    balance = balance + amount;
+    print('Deposited: $amount, Total: $balance');
+  }
+
+  void withdraw(double amount) {
+    if (amount <= balance) {
+      balance = balance - amount;
+      print('Withdrew: $amount, Remaining: $balance');
+    } else {
+      print('Insufficient balance!');
     }
   }
+}
+
+void main() {
+  BankAccount acc1 = BankAccount('101', 500.0);
+  acc1.deposit(100.0);
+  acc1.withdraw(200.0);
+  acc1.withdraw(900.0);
+
+  print('---');
+
+  BankAccount acc2 = BankAccount.zero('102');
+  acc2.deposit(150.0);
+  acc2.withdraw(50.0);
 }
