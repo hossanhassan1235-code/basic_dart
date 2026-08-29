1f220874e0ed35270af458734db66c2575b43b75
Hossam Hassan <hossanhassan1232@gmail.com>
2026-08-29 01:05:57 -0700

    update bank account code

--- bin/count_even_number.dart
+++ bin/count_even_number.dart
@@ -1,35 +1,31 @@
-class BankAccount {
-  String accountID;
-  double balance;
+class Holiday {
+  String name;
+  int day;
+  String month;
 
-  BankAccount(this.accountID, this.balance);
+  Holiday(this.name, this.day, this.month);
 
-  BankAccount.zero(this.accountID) : balance = 0.0 {}
-
-  void deposit(double amount) {
-    balance = balance + amount;
-    print('Deposited: $amount, Total: $balance');
+  bool inSameMonth(Holiday other) {
+    return this.month == other.month;
   }
 
-  void withdraw(double amount) {
-    if (amount <= balance) {
-      balance = balance - amount;
-      print('Withdrew: $amount, Remaining: $balance');
-    } else {
-      print('Insufficient balance!');
+  static double avgDate(List<Holiday> holidays) {
+    int sum = 0;
+    for (int i = 0; i < holidays.length; i++) {
+      sum = sum + holidays[i].day;
     }
+    return sum / holidays.length;
   }
 }
 
 void main() {
-  BankAccount acc1 = BankAccount('101', 500.0);
-  acc1.deposit(100.0);
-  acc1.withdraw(200.0);
-  acc1.withdraw(900.0);
+  Holiday h1 = Holiday('Independence Day', 4, 'July');
+  Holiday h2 = Holiday('Christmas', 25, 'December');
+  Holiday h3 = Holiday('New Year', 1, 'January');
 
-  print('---');
+  print(h1.inSameMonth(h2));
 
-  BankAccount acc2 = BankAccount.zero('102');
-  acc2.deposit(150.0);
-  acc2.withdraw(50.0);
+  List<Holiday> list = [h1, h2, h3];
+  double avg = Holiday.avgDate(list);
+  print('Average day: $avg');
 }
